e366196f90bedc396d8ce2f59706fd2df41580dd
Euclides Formica <eformica@gmail.com>
2023-04-12 08:27:46 -0300

    Correções

--- log_gen.py
+++ log_gen.py
@@ -2,19 +2,12 @@ from datetime import datetime
 
 """
 Gerador automatico de logs de execucao
-
 Modo de uso:
-
 #1 - Importar a classe LogGen e o metodo for_all_methods desse arquivo:
-
 from log_gen import log_gen
-
 #2 - Instanciar um objeto LogGen:
-
 logs = LogGen("Logs do script X") #A mensagem e opcional
-
 #3 - Usar os decorator for_all_methods e o metodo LogGen.decorator para gerar os logs sobre a classe desejada:
-
 @for_all_methods(logs.decorator)
 class ClasseDeExemplo:
 	def __init__(self):
@@ -38,18 +31,20 @@ def for_all_methods(decorator):
 class LogGen:
     def __init__(self, msg=None):
         #variavel onde os logs serao armazenados:
-        self.__logs = ""
+        self.__logs = []
+        self.n = 0
         
         if msg is not None:
             self._add_msg_log(msg)
     
     def _add_func_log(self, func, args, kwargs, status=None):
-        registro = f"{datetime.now()} Funcao: '{func.__name__}', args: {args[1:]}, kwargs: {kwargs} Status '{status}'"
-        self.__logs += "\n" + registro
+        registro = f"[{self.n}] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Funcao: '{func.__name__}', args: {args[1:]}, kwargs: {kwargs} Status '{status}'"
+        self.__logs.append(registro)
+        self.n += 1
         
     def _add_msg_log(self, msg):
-        registro = f"{datetime.now()} {msg}"
-        self.__logs += "\n" + registro
+        registro = f"[{self.n}] {datetime.now()} lprint: '{msg}'"
+        self.__logs.append(registro)
 
     def lprint(self, msg):
         print(msg)
@@ -57,21 +52,26 @@ class LogGen:
 
     def decorator(self, func):
         def wrapper(*args, **kwargs):
-            self._add_func_log(func=func, args=args, kwargs=kwargs, status="Iniciado")
+            self._add_func_log(func=func, args=args, kwargs=kwargs, status="STARTED")
             try:
                 res = func(*args, **kwargs)
                 self._add_func_log(func=func, args=args, kwargs=kwargs, status="OK")
                 return res
             
             except Exception as err:
-                self._add_func_log(func=func, args=args, kwargs=kwargs, status=err)
-                print(err)
+                self._add_func_log(func=func, args=args, kwargs=kwargs, status=f"'ERROR': {err}")
+                raise err
                 
         return wrapper
     
     def get_logs(self):
         self._add_msg_log("Log gerado.")
-        return self.__logs
+
+        res = ""
+        for k in self.__logs:
+            res += k + "\n"
+
+        return res
 
 #-----------------------TESTS-----------------------
 
